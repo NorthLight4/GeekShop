997d35efa3e011400cf31a1bc054c577a3b44e2c
NorthLight <alexcomandir@gmail.com>
2024-07-05 19:37:43 +0500

    Изменения после третьей лекции

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,17 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from mainapp import views as mainapp
+from django.conf.urls import include
 
 urlpatterns = [
     path('', mainapp.main, name='main'),
-    path('products/', mainapp.products, name='products'),
-    path('products/all/', mainapp.products, name='products_all'),
-    path('products/home/', mainapp.products, name='products_home'),
-    path('products/office/', mainapp.products, name='products_office'),
-    path('products/modern/', mainapp.products, name='products_modern'),
-    path('products/classic/', mainapp.products, name='products_classic'),
-
+    path('products/', include('mainapp.urls', namespace='products')),
     path('contact/', mainapp.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render
+from .models import ProductCategory, Product
 
 
 def main(request):
-    content = {
-        'title': 'Главная'
-    }
+    title = 'главная'
+    products = Product.objects.all()[:4]
+    content = {'title': title, 'products': products}
     return render(request, 'mainapp/index.html', content)
 
 
-def products(request):
-    links_menu = [
-        {'href': 'products_all', 'name':'все'},
-        {'href': 'products_home', 'name': 'домашние'},
-        {'href': 'products_office', 'name': 'офисные'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классические'},
-    ]
+
+def products(request, pk=None):
+    title = 'продукты'
+    same_products = Product.objects.all()[:4]
+    links_menu = ProductCategory.objects.all()
     content = {
-        'title': 'Продукты',
-        'links_menu': links_menu
+        'title': title,
+        'links_menu': links_menu,
+        'same_products': same_products
     }
     return render(request, 'mainapp/products.html', content)
 
@@ -28,5 +27,3 @@ def contact(request):
         'title': 'Контакты'
     }
     return render(request, 'mainapp/contact.html', content)
-
-# Create your views here.
